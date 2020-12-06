940e293c103153eb225159cbe2b9b65fc9b4a57c
Daniel O. Souza <daniell.oliveirra@gmail.com>
2020-12-05 21:23:02 -0300

    URL server

--- src/shared/providers/api.js
+++ src/shared/providers/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import ViewUI from 'view-design';
 
 const api = axios.create({
-   baseURL: 'http://localhost:3333',
+   baseURL: 'http://206.189.65.249:3333',
 });
 
 api.interceptors.request.use(
